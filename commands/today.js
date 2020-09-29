f47bc5c0d4c970841ebc5f756c2cfd907c0f8ecd
Logan Kuzyk <logan@kuzyk.net>
2020-09-29 16:58:08 -0700

    Fix response when there are no due dates

--- commands/today.js
+++ commands/today.js
@@ -26,7 +26,15 @@ module.exports.execute = async ({ bot, msg, input, channel, course }) => {
       range: "A1:E",
     })
     .then((res) => {
-      if (!res.data.values) return;
+      console.log("hello");
+      if (!res.data.values) {
+        if (msg) {
+          msg.reply(output);
+        } else if (channel) {
+          channel.send(output);
+        }
+        return;
+      }
       output = "";
       output += `Here are the assignments due in the next 24 hours for ${course}\n`;
       let weekFromNow = new Date(Date.now() + 24 * 60 * 60 * 1000);
@@ -41,8 +49,6 @@ module.exports.execute = async ({ bot, msg, input, channel, course }) => {
         output += `${row[4]} is due ${new Date(Number(row[0]))}\n`;
       }
       console.log(output);
-      if (msg) msg.reply(output);
-      else if (channel) channel.send(output);
     });
 
   return;
--- commands/upcoming.js
+++ commands/upcoming.js
@@ -26,7 +26,14 @@ module.exports.execute = async ({ bot, msg, input, channel, course }) => {
       range: "A1:E",
     })
     .then((res) => {
-      if (!res.data.values) return;
+      if (!res.data.values) {
+        if (msg) {
+          msg.reply(output);
+        } else if (channel) {
+          channel.send(output);
+        }
+        return;
+      }
       output = "";
       output += `Here are the assignments due in the next 7 days for ${course}\n`;
       let weekFromNow = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
@@ -41,8 +48,6 @@ module.exports.execute = async ({ bot, msg, input, channel, course }) => {
         output += `${row[4]} is due ${new Date(Number(row[0]))}\n`;
       }
       console.log(output);
-      if (msg) msg.reply(output);
-      else if (channel) channel.send(output);
     });
 
   return;
