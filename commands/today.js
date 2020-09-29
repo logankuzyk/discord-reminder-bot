ab57a70b5c0019659b19f226bb0f3163304ebb68
Logan Kuzyk <logan@kuzyk.net>
2020-09-28 20:46:51 -0700

    Command boilerplate

---
+++ commands/today.js
@@ -0,0 +1,5 @@
+const Discord = require("discord.js");
+
+module.exports.about = "Display the assignments that are due today.";
+
+module.exports.execute = async (msg, input) => {};
---
+++ commands/upcoming.js
@@ -0,0 +1,5 @@
+const Discord = require("discord.js");
+
+module.exports.about = "List the next 7 days of assignments.";
+
+module.exports.execute = async (msg) => {};
