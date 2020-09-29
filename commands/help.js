e384c3e6fb1ac34533311d06fe43117d5e582896
Logan Kuzyk <logan@kuzyk.net>
2020-09-28 20:04:14 -0700

    Add help command

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+
+module.exports.about = "List available commands.";
+
+module.exports.execute = async (msg) => {
+  let output = "";
+  let files = fs.readdirSync(__dirname).filter((file) => file.endsWith(".js"));
+  for (let file of files) {
+    let command = file.substring(0, file.indexOf("."));
+    try {
+      let module = require(`${__dirname}/${command}`);
+      if (module) output += `\n \`\`\$${command}: ${module.about}\`\``;
+    } catch (err) {
+      console.log(err);
+      continue;
+    }
+  }
+  if (output) {
+    msg.reply(output);
+  }
+};
