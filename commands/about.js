3eececb07eb602567ad6bde7a82b020467c418c7
Logan Kuzyk <logan@kuzyk.net>
2020-10-04 18:02:11 -0700

    Comedy

--- commands/about.js
+++ commands/about.js
@@ -4,6 +4,6 @@ module.exports.help = "Example command: ``$about``";
 
 module.exports.execute = async ({ bot, msg }) => {
   msg.reply(
-    "Store due dates and reminders for courses. Developed by C1RRU5. https://github.com/logankuzyk/discord-reminder-bot"
+    "Store due dates and reminders for courses. Developed by C1RRU5. Want to have a closer look at the dumpster fire that stores all your important due dates? Check out https://github.com/logankuzyk/discord-reminder-bot"
   );
 };
