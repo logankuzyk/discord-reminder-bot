17c67a4a3d9352c7277b1b2641e10c1001ede129
Logan Kuzyk <logan@kuzyk.net>
2020-10-07 11:20:59 -0700

    More ERROR HANDLING

--- commands/remind.js
+++ commands/remind.js
@@ -26,63 +26,68 @@ module.exports.execute = async ({ bot, msg, input }) => {
     } = /^(?<date>\S+)( at ((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*))*( for (?<course>([(a-z]{3,4}-([0-9]{2})\w$)))*$/g.exec(
       input
     );
-  } catch (err) {
-    throw new Error(
-      "Your command doesn't match the format. Try ``$help remind`` if you're stuck."
-    );
-  }
 
-  hour = Number(hour);
-  minute = Number(minute);
-  if (!course && msg.channel.name != "bot-commands") {
-    course = msg.channel.name;
-  } else if (msg.channel.name == "bot-commands" && !course) {
-    throw new Error(
-      "Since you're in the bot-commands channel, you're going to need to provide the course for this due date."
-    );
-  }
-  if (hour && timeSuffix) {
-    if (timeSuffix.toLowerCase() == "pm" && hour != 12) {
-      hour += 12;
-    }
-  } else if (hour == 24) {
-    hour = 0;
-  }
-  if (hour && minute) {
-    date += ` ${hour}:${minute}`;
-  } else if (hour) {
-    date += ` ${hour}:00`;
-  } else {
-    date += ` 00:00`;
-  }
-  date = new Date(date);
-  console.log(date.toString());
-  if (date == "Invalid Date") throw new Error("Date is invalid or not present");
-  if (msg.createdTimestamp > date.getTime())
-    throw new Error("Can't change history");
-  let id = await randomstring.generate({
-    length: 6,
-    readable: true,
-    charset: "alphabetic",
-  });
-  sheets.spreadsheets.values
-    .append({
-      spreadsheetId: process.env.SHEET_ID,
-      valueInputOption: "RAW",
-      insertDataOption: "INSERT_ROWS",
-      range: "A2:A",
-      resource: {
-        range: "A2:A",
-        values: [[id, date.getTime(), course, msg.author.id, "reminder"]],
-      },
-    })
-    .then((res) => {
-      msg.reply(
-        `Reminder added! I will send you a PM with ${course}'s upcoming due dates at ${date.toString()}. \nThis reminder's unique ID is \`\`${id}\`\``
+    hour = Number(hour);
+    minute = Number(minute);
+    if (!course && msg.channel.name != "bot-commands") {
+      course = msg.channel.name;
+    } else if (msg.channel.name == "bot-commands" && !course) {
+      throw new Error(
+        "Since you're in the bot-commands channel, you're going to need to provide the course for this due date."
       );
-      return bot.loadRange(res.data.updates.updatedRange);
-    })
-    .catch((err) => {
-      throw new Error(`Something went wrong with Google Sheets ${err}`);
+    }
+    if (hour && timeSuffix) {
+      if (timeSuffix.toLowerCase() == "pm" && hour != 12) {
+        hour += 12;
+      }
+    } else if (hour == 24) {
+      hour = 0;
+    }
+    if (hour && minute) {
+      date += ` ${hour}:${minute}`;
+    } else if (hour) {
+      date += ` ${hour}:00`;
+    } else {
+      date += ` 00:00`;
+    }
+    date = new Date(date);
+    console.log(date.toString());
+    if (date == "Invalid Date")
+      throw new Error("Date is invalid or not present");
+    if (msg.createdTimestamp > date.getTime())
+      throw new Error("Can't change history");
+    let id = await randomstring.generate({
+      length: 6,
+      readable: true,
+      charset: "alphabetic",
     });
+    sheets.spreadsheets.values
+      .append({
+        spreadsheetId: process.env.SHEET_ID,
+        valueInputOption: "RAW",
+        insertDataOption: "INSERT_ROWS",
+        range: "A2:A",
+        resource: {
+          range: "A2:A",
+          values: [[id, date.getTime(), course, msg.author.id, "reminder"]],
+        },
+      })
+      .then((res) => {
+        msg.reply(
+          `Reminder added! I will send you a PM with ${course}'s upcoming due dates at ${date.toString()}. \nThis reminder's unique ID is \`\`${id}\`\``
+        );
+        return bot.loadRange(res.data.updates.updatedRange);
+      })
+      .catch((err) => {
+        throw new Error(`Something went wrong with Google Sheets ${err}`);
+      });
+  } catch (err) {
+    if (err.name == "TypeError") {
+      throw new Error(
+        "Your command doesn't match the format. Try $help add if you're stuck."
+      );
+    } else {
+      throw new Error(err.message);
+    }
+  }
 };
