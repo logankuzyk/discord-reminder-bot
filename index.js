5a630ffa7aaa2885c21ecb3738b81f7dc082d2e2
Logan Kuzyk <logan@kuzyk.net>
2020-10-26 20:35:48 -0700

    bot.commands resolves to a map now

--- index.js
+++ index.js
@@ -1,88 +1,46 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const cron = require("cron");
-const { google } = require("googleapis");
 const dotenv = require("dotenv").config();
+const Storage = require("./storage");
+const Schedule = require("./schedule");
 
 const bot = new Discord.Client();
-const auth = new google.auth.GoogleAuth({
-  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
-google.options({
-  auth: auth,
-});
-const sheets = google.sheets({
-  version: "v4",
-  credentials: auth,
-});
-
-bot.loadRange = (range) => {
-  sheets.spreadsheets.values
-    .get({
-      spreadsheetId: process.env.SHEET_ID,
-      range: range,
-    })
-    .then((res) => {
-      if (!res.data.values) {
-        return;
-      }
-      for (let row of res.data.values) {
-        let date = new Date(Number(row[1]));
-        if (date.getTime() > Date.now()) {
-          if (row[4] == "reminder") {
-            let user = bot.users.cache.get(row[3]);
-            user.createDM().then((dmChannel) => {
-              bot.addJob(date, bot.commands.get("upcoming").execute, {
-                bot: bot,
-                channel: dmChannel,
-                course: row[2],
-              });
-            });
-            console.log(`Added reminder on ${date.toString()}`);
-          }
-          console.log(`Added due date on ${date.toString()}`);
-        }
-      }
-    });
-};
-
-bot.addJob = (date, command, options) => {
-  let job = new cron.CronJob(
-    date,
-    () => {
-      command(options);
-    },
-    null,
-    true,
-    "America/Los_Angeles"
-  );
-  job.start();
-};
 
 bot.on("ready", () => {
-  bot.commands = new Discord.Collection();
-  let files = fs
-    .readdirSync("./commands/")
-    .filter((file) => file.endsWith(".js"));
-  for (let file of files) {
-    let command = file.substring(0, file.indexOf("."));
-    let module = require(`./commands/${command}`);
-    bot.commands.set(file.substring(0, file.indexOf(".")), module);
-    console.log(`Loaded ${file}`);
-  }
-  let channels = bot.channels.cache.filter(
-    (channel) =>
-      channel.type == "text" &&
-      channel.name.match(/^([a-z]{3,4})-([0-9]{2})\w$/g)
-  );
-  for (let channel of channels) {
-    bot.addJob("0 0 0 * * *", bot.commands.get("upcoming").execute, {
-      bot: bot,
-      channel: channel[1],
-    });
-  }
-  bot.loadRange("A2:E");
+  new Promise((resolve, reject) => {
+    try {
+      let commands = new Map();
+      let files = fs
+        .readdirSync("./commands/")
+        .filter((file) => file.endsWith(".js"));
+      for (let file of files) {
+        let command = file.substring(0, file.indexOf("."));
+        let module = require(`./commands/${command}`);
+        commands.set(file.substring(0, file.indexOf(".")), module);
+        console.log(`Loaded ${file}`);
+      }
+      resolve(commands);
+    } catch (err) {
+      reject(err);
+    }
+  }).then((commands) => {
+    module.exports.commands = commands;
+    bot.commands = commands;
+    // console.log(module.exports);
+    const storage = new Storage();
+    const schedule = new Schedule();
+    storage.tasks.then(schedule.addJob);
+    let channels = bot.channels.cache.filter(
+      (channel) =>
+        channel.type == "text" &&
+        channel.name.match(/^([a-z]{3,4})-([0-9]{2})\w$/g)
+    );
+    for (let channel of channels) {
+      schedule.addJob({
+        courseName: channel[1].id,
+      });
+    }
+  });
 });
 
 bot.on("message", async (msg) => {
@@ -106,6 +64,7 @@ bot.on("message", async (msg) => {
     groups: { command, input },
   } = /^\$(?<command>\w+)( )*(?<input>(.+) *)*$/g.exec(msg.content);
   try {
+    console.log(bot.commands);
     bot.commands
       .get(command)
       .execute({ bot: bot, msg: msg, input: input })
