f8cd648e5d502a4c9738d8851feefb2e197c58ed
Logan Kuzyk <logan@kuzyk.net>
2020-10-29 18:11:24 -0700

    Add command regex

--- bin/regex.js
+++ bin/regex.js
@@ -1,5 +1,6 @@
 module.exports = new Map([
   ["course", /^(?<course>([(a-z]{3,4}-([0-9]{2})\w$))/g],
+  ["command", /^\$(?<command>\w+)( )*(?<input>(.+) *)*$/g],
   [
     "dueDate",
     /^(?<note>(\w( )*)+) on (?<date>\S+)( at ((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*))*( for (?<course>([(a-z]{3,4}-([0-9]{2})\w$)))*$/g,
