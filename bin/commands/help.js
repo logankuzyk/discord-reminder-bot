a0634a3a1a623bdeacc4235b76fbbfb64a6fd42f
Logan Kuzyk <logan@kuzyk.net>
2020-11-09 15:10:31 -0800

    New command format

--- bin/commands/help.js
+++ bin/commands/help.js
@@ -1,30 +1,34 @@
+const Command = require("../command");
 const fs = require("fs");
 
-module.exports.about = "List available commands.";
+const help = new Command();
 
-module.exports.help = "Example command: ``$help``";
+help.about = "List available commands.";
 
-module.exports.execute = async ({ bot, msg, input }) => {
-  let output =
-    "Remember, you only need to specify the course when you're in the bot-commands channel. When in a course channel, you can only interact with that course's due dates.";
+help.help = "Example command: ``$help``";
+
+help.params = [];
+
+help.execute = async () => {
+  let body = "```";
   let files = fs.readdirSync(__dirname).filter((file) => file.endsWith(".js"));
-  if (input) {
-    msg.reply(bot.commands.get(input).help);
-    return;
-  }
   for (let file of files) {
     let command = file.substring(0, file.indexOf("."));
     try {
       let module = require(`${__dirname}/${command}`);
-      if (module) output += `\n \`\`\$${command}: ${module.about}\`\``;
+      if (module) body += `\n\$${command}: ${module.about}`;
     } catch (err) {
       console.log(err);
       continue;
     }
   }
-  if (output) {
-    output +=
-      "\nFor more information about a command, type ``$help {command name}``.";
-    msg.reply(output);
+  if (body != "```") {
+    body += "```";
   }
+  return {
+    embed: { title: "Assignment Reminder's Commands", description: body },
+    complete: true,
+  };
 };
+
+module.exports = help;
