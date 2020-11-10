8524cc7038ff4fcb57e36ab6511a34b0844525d9
Logan Kuzyk <logan@kuzyk.net>
2020-11-10 15:42:04 -0800

    Add some prompts for parameters

---
+++ bin/prompts.js
@@ -0,0 +1,14 @@
+module.exports = new Map([
+  [
+    "courseName",
+    "Okay, what course is the reminder for? Respond in the same format as the course channels, like math-200 for example.",
+  ],
+  [
+    "date",
+    "Okay, what day do you want to be reminded? Respond in the format YYYY-MM-DD.",
+  ],
+  [
+    "time",
+    "Okay, what time do you want to be reminded on that day? Respond in the format HH:MM for 24 hour format, or HH:MMpm for 12 hour format.",
+  ],
+]);
