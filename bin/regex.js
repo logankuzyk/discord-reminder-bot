550b2563995ff9a3e8b0ecac18080e3482e7e0ef
Logan Kuzyk <logan@kuzyk.net>
2020-11-10 19:02:31 -0800

    Add some regex for commands

--- bin/regex.js
+++ bin/regex.js
@@ -1,6 +1,8 @@
 module.exports = new Map([
   ["course", /^(?<course>([(a-z]{3,4}-([0-9]{2})\w$))/],
   ["command", /^\$(?<command>\w+)/],
+  ["token", /(?<token>\w+)/g],
+  ["time", /((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*)*/],
   [
     "dueDate",
     /^(?<note>(\w( )*)+) on (?<date>\S+)( at ((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*))*( for (?<course>([(a-z]{3,4}-([0-9]{2})\w$)))*$/,
