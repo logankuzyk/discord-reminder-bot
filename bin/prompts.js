67a7e68075afc4f7748baf5aec3181bdbbc89dcd
Logan Kuzyk <logan@kuzyk.net>
2020-11-10 19:02:17 -0800

    Clarify prompts

--- bin/prompts.js
+++ bin/prompts.js
@@ -1,14 +1,14 @@
 module.exports = new Map([
   [
     "courseName",
-    "Okay, what course is the reminder for? Respond in the same format as the course channels, like math-200 for example.",
+    'Okay, what course is the reminder for? Respond in the same format as the course channels, like $math-200 for example. You need to start the message with "$" for the bot to see it.',
   ],
   [
     "date",
-    "Okay, what day do you want to be reminded? Respond in the format YYYY-MM-DD.",
+    'Okay, what day do you want to be reminded? Respond in the format $YYYY-MM-DD. You need to start the message with "$" for the bot to see it.',
   ],
   [
     "time",
-    "Okay, what time do you want to be reminded on that day? Respond in the format HH:MM for 24 hour format, or HH:MMpm for 12 hour format.",
+    'Okay, what time do you want to be reminded on that day? Respond in the format $HH:MM for 24 hour format, or $HH:MMpm for 12 hour format. You need to start the message with "$" for the bot to see it.',
   ],
 ]);
