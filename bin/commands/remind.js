f5e6f5e7048623139065e31c2b61cce8c53de9e5
Logan Kuzyk <logan@kuzyk.net>
2020-11-11 12:32:44 -0800

    Updated to work with new system

--- bin/commands/remind.js
+++ bin/commands/remind.js
@@ -1,96 +1,115 @@
-const { google } = require("googleapis");
+const Command = require("../command");
+const regex = require("../regex");
+const prompts = require("../prompts");
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
-google.options({
-  auth: auth,
-});
-const sheets = google.sheets({
-  version: "v4",
-  credentials: auth,
-});
+const remind = new Command();
 
-module.exports.about =
-  "Get reminded about a classes' due dates at a certain time.";
-
-module.exports.help =
+remind.about = "Get reminded about a course's due dates at a certain time.";
+remind.help =
   "Example command: ``$remind 2020-09-30 at 12pm for math-200`` \nGeneral form: ``$remind [YYYY-MM-DD] (at [HH:mm])? (for [course])?``";
+remind.params = ["courseName", "date", "time"];
 
-module.exports.execute = async ({ bot, msg, input }) => {
-  try {
-    let {
-      groups: { date, hour, minute, timeSuffix, course },
-    } = /^(?<date>\S+)( at ((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*))*( for (?<course>([(a-z]{3,4}-([0-9]{2})\w$)))*$/g.exec(
-      input
-    );
-
-    hour = Number(hour);
-    minute = Number(minute);
-    if (!course && msg.channel.name != "bot-commands") {
-      course = msg.channel.name;
-    } else if (msg.channel.name == "bot-commands" && !course) {
-      throw new Error(
-        "Since you're in the bot-commands channel, you're going to need to provide the course for this due date."
+remind.execute = async (user, tokens) => {
+  let nextParam;
+  let givenParams = {};
+  let remainingParams = [];
+  if (user && user.ongoingCommand != "null") {
+    givenParams = user.givenParams;
+    remainingParams = new Array(...user.remainingParams);
+    console.log(`Remaining: ${remainingParams}`);
+    console.log(`Given: ${givenParams}`);
+    console.log(`Looking for ${user.nextParam}`);
+    if (user.nextParam == "courseName") {
+      let courseName = tokens.filter(
+        (token) => regex.get("course").exec(token) !== null
       );
-    }
-    if (hour && timeSuffix) {
-      if (timeSuffix.toLowerCase() == "pm" && hour != 12) {
-        hour += 12;
+      if (courseName) {
+        console.log(`Found courseName ${courseName}`);
+        [givenParams.courseName] = courseName;
+        remainingParams.splice(remainingParams.indexOf("courseName"), 1);
+        nextParam = remainingParams[0];
       }
-    } else if (hour == 24) {
-      hour = 0;
-    }
-    if (hour && minute) {
-      date += ` ${hour}:${minute}`;
-    } else if (hour) {
-      date += ` ${hour}:00`;
-    } else {
-      date += ` 00:00`;
-    }
-    date = new Date(date);
-    console.log(date.toString());
-    if (date == "Invalid Date")
-      throw new Error("Date is invalid or not present");
-    if (msg.createdTimestamp > date.getTime())
-      throw new Error("Can't change history");
-    msg
-      .reply(
-        `Reminder added! I will send you a PM with ${course}'s upcoming due dates at ${date.toString()}.`
-      )
-      .then((reply) => {
-        sheets.spreadsheets.values
-          .append({
-            spreadsheetId: process.env.SHEET_ID,
-            valueInputOption: "RAW",
-            insertDataOption: "INSERT_ROWS",
-            range: "A2:A",
-            resource: {
-              range: "A2:A",
-              values: [
-                [
-                  reply.id,
-                  date.getTime(),
-                  course,
-                  msg.author.id,
-                  "reminder",
-                  1,
-                ],
-              ],
-            },
-          })
-          .then((res) => {
-            return bot.loadRange(res.data.updates.updatedRange);
-          });
-      });
-  } catch (err) {
-    if (err.name == "TypeError") {
-      throw new Error(
-        "Your command doesn't match the format. Try $help add if you're stuck."
+    } else if (user.nextParam == "date") {
+      tokens = tokens.filter((token) => regex.get("date").exec(token));
+      let date = tokens.filter((token) => new Date(token) != "Invalid Date");
+      if (date.length > 0) {
+        // console.log(date);
+        console.log(`Found date ${date}`);
+        [givenParams.date] = date;
+        remainingParams.splice(remainingParams.indexOf("date"), 1);
+        nextParam = remainingParams[0];
+      }
+    } else if (user.nextParam == "time") {
+      let time = tokens.filter(
+        (token) => regex.get("time").exec(token).groups.hour
       );
-    } else {
-      throw new Error(err.message);
+      if (time) {
+        console.log(`Found time ${time}`);
+        givenParams.time = regex.get("time").exec(time).groups;
+        remainingParams.splice(remainingParams.indexOf("time"), 1);
+        nextParam = remainingParams[0];
+      }
     }
+  } else {
+    // Entry point of command, user has not interacted with bot before.
+    remainingParams = new Array(...remind.params);
+    let courseName = tokens.filter(
+      (token) => regex.get("course").exec(token) !== null
+    )[0];
+    if (courseName) {
+      givenParams.courseName = courseName;
+      remainingParams.splice(remainingParams.indexOf("courseName"), 1);
+    }
+    nextParam = remainingParams[0];
   }
+  let body;
+  let complete;
+  let taskTime;
+  if (nextParam) {
+    body = prompts.get(nextParam);
+    complete = false;
+  } else {
+    // Routine on completion
+    // Time parsing
+    taskTime = givenParams.date;
+    givenParams.time.hour = Number(givenParams.time.hour);
+    givenParams.time.minute = Number(givenParams.time.minute);
+    if (givenParams.time.hour && givenParams.time.timeSuffix) {
+      if (
+        givenParams.time.timeSuffix.toLowerCase() == "pm" &&
+        givenParams.time.hour != 12
+      ) {
+        givenParams.time.hour += 12;
+      }
+    } else if (givenParams.time.hour == 24) {
+      givenParams.time.hour = 0;
+    }
+    if (givenParams.time.hour && givenParams.time.minute) {
+      taskTime += ` ${givenParams.time.hour}:${givenParams.time.minute}`;
+    } else if (givenParams.time.hour) {
+      taskTime += ` ${givenParams.time.hour}:00`;
+    } else {
+      taskTime += ` 00:00`;
+    }
+    console.log(taskTime);
+    taskTime = new Date(taskTime);
+    if (taskTime == "Invalid Date")
+      throw new Error("Date is invalid or not present");
+    if (Date.now() > taskTime.getTime())
+      throw new Error("Can't change history");
+    body = `Reminder added! I will send you a PM with ${
+      givenParams.courseName
+    }'s due dates on ${taskTime.toString()}`;
+    complete = true;
+  }
+  return {
+    embed: { title: "New Reminder", description: body },
+    complete: complete,
+    givenParams: JSON.stringify(givenParams),
+    remainingParams: remainingParams,
+    nextParam: nextParam,
+    taskTime: taskTime,
+  };
 };
+
+module.exports = remind;
