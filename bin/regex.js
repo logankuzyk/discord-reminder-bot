0f197c0b6f092d9f31f5ca8022c2eb97d73d93e5
Logan Kuzyk <logan@kuzyk.net>
2020-11-15 12:20:24 -0800

    Fix time pattern

--- bin/regex.js
+++ bin/regex.js
@@ -2,7 +2,7 @@ module.exports = new Map([
   ["course", /^(?<course>([(a-z]{3,4}-([0-9]{2})\w$))/],
   ["command", /^\$(?<command>\w+)/],
   ["token", /\$*(?<token>\S+)/g],
-  ["time", /((?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*)*/],
+  ["time", /^(?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*/],
   ["date", /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/],
   [
     "dueDate",
