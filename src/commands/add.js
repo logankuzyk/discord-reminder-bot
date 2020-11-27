21c078ab07190374434245adc02d4a7f3e6d23a7
Logan Kuzyk <logan@kuzyk.net>
2020-11-26 21:40:17 -0800

    Use new command constructor

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,61 +1,8 @@
 const Command = require("../command");
-const regex = require("../regex");
-const prompts = require("../prompts");
-const paramGetter = require("../paramGetter");
 
-const add = new Command();
-
-add.about = "Get reminded about a course's due dates at a certain time.";
-add.params = ["courseName", "date", "time", "memo"];
-
-add.execute = async (user, tokens) => {
-  let nextParam;
-  let givenParams = {};
-  let remainingParams = [];
-  let ongoingCommand = null;
-  let body;
-  let paramReadSuccess;
-  if (user && user.ongoingCommand != "null") {
-    ongoingCommand = user.ongoingCommand;
-    let params = await paramGetter(user, tokens);
-    if (params) {
-      nextParam = params.nextParam;
-      givenParams = params.givenParams;
-      remainingParams = params.remainingParams;
-      paramReadSuccess = true;
-      body = prompts.get(nextParam);
-    } else {
-      nextParam = user.nextParam;
-      givenParams = user.givenParams;
-      remainingParams = user.remainingParams;
-      paramReadSuccess = false;
-    }
-  } else {
-    // Entry point of command, user has not interacted with bot before.
-    paramReadSuccess = true;
-    remainingParams = new Array(...add.params);
-    let courseName = tokens.filter(
-      (token) => regex.get("course").exec(token) !== null
-    )[0];
-    if (courseName) {
-      givenParams.courseName = courseName;
-      remainingParams.splice(remainingParams.indexOf("courseName"), 1);
-    }
-    nextParam = remainingParams[0];
-  }
-  let complete;
-  let task;
-  if (paramReadSuccess) {
-    body = prompts.get(nextParam);
-  } else {
-    body =
-      "I wasn't able to read that, please try again.\n\n" +
-      prompts.get(nextParam);
-  }
-  if (nextParam) {
-    complete = false;
-  } else {
-    // Routine on completion
+const add = new Command(
+  ["courseName", "date", "time", "memo"],
+  async (givenParams) => {
     givenParams.time = new Date(givenParams.time);
     task = {
       executeDate: givenParams.time.getTime(),
@@ -68,16 +15,16 @@ add.execute = async (user, tokens) => {
     body = `Due date added! ${
       givenParams.memo
     } is due on ${givenParams.time.toString()} for ${givenParams.courseName}.`;
-    complete = true;
-  }
-  return {
-    embed: { title: "New Due Date", description: body },
-    complete: complete,
-    givenParams: JSON.stringify(givenParams),
-    remainingParams: remainingParams,
-    nextParam: nextParam,
-    task: task,
-  };
-};
+    return {
+      embed: { title: "New Due Date", description: body },
+      complete: true,
+      givenParams: JSON.stringify(givenParams),
+      task: task,
+    };
+  },
+  "New Due Date",
+  "Set a due date for a certain class for others to access through the bot.",
+  "add"
+);
 
 module.exports = add;
