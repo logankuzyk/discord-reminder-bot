0082f26abe33c5284af155ba4755b5c8c85f8085
Logan Kuzyk <logan@kuzyk.net>
2020-11-29 19:48:16 -0800

    Make prompts generic

--- src/prompts.js
+++ src/prompts.js
@@ -1,15 +1,15 @@
 module.exports = new Map([
   [
     "courseName",
-    'What **course** is the reminder for?\nRespond in the same format as the course channels, like **$math-200** for example.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
+    'What **course** is this for?\nRespond in the same format as the course channels, like **$math-200** for example.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
   ],
   [
     "date",
-    'What **day** do you want to be reminded?\nRespond in the format **$YYYY-MM-DD**.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
+    'What **day**?\nRespond in the format **$YYYY-MM-DD**.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
   ],
   [
     "time",
-    'What **time** do you want to be reminded on that day?\nRespond in the format **$HH:MM** for 24 hour format, or **$HH:MM[am/pm]** for 12 hour format.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
+    'What **time** do you want on that day?\nRespond in the format **$HH:MM** for 24 hour format, or **$HH:MM[am/pm]** for 12 hour format.\nYou need to start the message with "$" for the bot to see it.\nType $cancel to escape.',
   ],
   [
     "memo",
