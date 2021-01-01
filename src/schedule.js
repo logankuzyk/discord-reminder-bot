997f1c5ed581ff497f7ee7226cc3731f28a36392
Logan Kuzyk <logan@kuzyk.net>
2020-12-31 18:55:50 -0800

    Fix reminders

--- src/schedule.js
+++ src/schedule.js
@@ -22,17 +22,19 @@ class Schedule {
     }
     // Execution defaults to everyday at midnight. This is for the daily class reminders.
     let executeDate = "0 0 0 * * *";
-    if (task.executeDate) {
+    if (task.executeDate && task.taskType == "assignment") {
       executeDate = new Date(Number(task.executeDate));
       executeDate.setHours(executeDate.getHours() - 4);
       if (executeDate.getTime() - Date.now() < 0) return;
+    } else if (task.executeDate && task.taskType == "reminder") {
+      executeDate = new Date(Number(task.executeDate));
     }
-    let command = index.commands.get("upcoming").execute;
+    let command = index.commands.get("upcoming").onParamFulfilment;
 
     let job = new cron.CronJob(
       executeDate,
       () => {
-        command({ courseName: [task.courseName], scheduled: true }).then(
+        command({ courseName: task.courseName, scheduled: true }).then(
           (context) => {
             let channel = index.channels.cache.get(task.channelId);
             let embed = new Discord.MessageEmbed(context.embed);
