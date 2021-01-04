a658ad8e868c91d6f45e06f09db5813cec66fd43
Logan Kuzyk <logan@kuzyk.net>
2021-01-04 14:48:29 -0800

    Remind course channel 24 hours before due date, and at time of due date

--- src/schedule.js
+++ src/schedule.js
@@ -12,48 +12,42 @@ class Schedule {
     }
   }
   addMiscJob = async (time, callback) => {
-    let job = new cron.CronJob(time, callback, null, true, this.tz);
-    return job;
+    if (time instanceof Date && time.getTime() > Date.now()) {
+      let job = new cron.CronJob(time, callback, null, true, this.tz);
+      return job;
+    } else {
+      console.log("Job is in the past, skipping");
+      return;
+    }
   };
 
   addCourseReminder = async (task) => {
     if (task instanceof Map) {
       return task.forEach(this.addCourseReminder);
     }
-    // Execution defaults to everyday at midnight. This is for the daily class reminders.
-    let executeDate = "0 0 0 * * *";
-    if (task.executeDate && task.taskType == "assignment") {
-      executeDate = new Date(Number(task.executeDate));
-      executeDate.setHours(executeDate.getHours() - 4);
-      if (executeDate.getTime() - Date.now() < 0) return;
-    } else if (task.executeDate && task.taskType == "reminder") {
-      executeDate = new Date(Number(task.executeDate));
-    }
-
-    if (executeDate.getTime() < Date.now()) {
-      console.log("Date has passed, skipping job");
-      return;
-    }
+    task.executeDate = Number(task.executeDate);
 
     let command = index.commands.get("upcoming").onParamFulfilment;
+    let callback = () => {
+      command({ courseName: task.courseName, scheduled: true }).then(
+        (context) => {
+          let channel = index.channels.cache.get(task.channelId);
+          let embed = new Discord.MessageEmbed(context.embed);
+          embed.setColor("ffc83d");
+          channel.send(embed);
+        }
+      );
+    };
 
-    let job = new cron.CronJob(
-      executeDate,
-      () => {
-        command({ courseName: task.courseName, scheduled: true }).then(
-          (context) => {
-            let channel = index.channels.cache.get(task.channelId);
-            let embed = new Discord.MessageEmbed(context.embed);
-            embed.setColor("ffc83d");
-            channel.send(embed);
-          }
-        );
-      },
-      null,
-      true,
-      this.tz
-    );
-    return job;
+    let dayBefore;
+    if (task.executeDate && task.taskType == "assignment") {
+      dayBefore = new Date(task.executeDate);
+      dayBefore.setHours(dayBefore.getHours() - 24);
+      await this.addMiscJob(dayBefore, callback);
+    }
+
+    await this.addMiscJob(new Date(task.executeDate), callback);
+    return;
   };
 }
 
