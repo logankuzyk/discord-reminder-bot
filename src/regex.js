d2dc3215697c12471fc2891859f5c7938e55d924
Logan Kuzyk <logan@kuzyk.net>
2021-01-04 16:47:44 -0800

    ece-250-linear-squircuits

--- src/regex.js
+++ src/regex.js
@@ -1,5 +1,8 @@
 module.exports = new Map([
-  ["course", /^(?<course>([a-z]{3,4}(-([0-9]{3})[a-z]*)+))$/],
+  [
+    "course",
+    /^(?<course>([a-z]{3,4}(-([0-9]{3})(-linear-squircuits)*[a-z]*)+))$/,
+  ],
   ["command", /^\$(?<command>\w+)/],
   ["token", /\$*(?<token>\S+)/g],
   ["time", /^(?<hour>\d{1,2})(:(?<minute>\d{2}))*(?<timeSuffix>[a-z]{2})*/],
